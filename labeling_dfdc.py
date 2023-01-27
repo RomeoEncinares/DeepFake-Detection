ca3b6e1e99956029cf0f6f87d67ffce2aeab3c20
Romeo Encinares <romeoncrs@gmail.com>
2023-01-27 17:27:49 +0800

    Implemented argument parser for running label dfdc function via command-line interface

--- labeling_dfdc.py
+++ labeling_dfdc.py
@@ -1,25 +1,46 @@
 import json
 import pandas as pd
 import os
+import argparse
+import sys
 
-directory = "dataset_processed/train/"
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--metadata', type=str, help='JSON metadata directory', required=True)
+    parser.add_argument('--datasetRoot', type=str, help='Dataset root directory', required=True)
+    parser.add_argument('--dataframeRoot', type=str, help='CSV dataframe output directory', required=True)
 
-# load the JSON file
-with open('dataset/train_sample_videos/metadata.json') as json_file:
-    videos = json.load(json_file)
+    return parser.parse_args(argv)
 
-# create a list to hold the data for the dataframe
-data = []
+def main(argv):
+    args = parse_args(argv)
 
-# loop through the videos and extract the frames
-for video_name, video_info in videos.items():
-    label = video_info['label']
-    split = video_info['split']
-    original = video_info['original']
-#     print(video_name.rsplit('.', 1)[0])
-    for frame_name in os.listdir(directory):
-        if video_name.rsplit('.', 1)[0] in frame_name:
-             data.append({'video_name': video_name,'frame_name': frame_name, 'label': label, 'split': split})
-        
-df = pd.DataFrame(data, columns=['video_name', 'frame_name', 'label', 'split'])
-df.to_csv('df.csv')
+    # dataset_processed/
+    metadata_dir = args.metadata
+    dataset_dir = args.datasetRoot
+    dataframe_dir = args.dataframeRoot
+
+    # load the JSON file
+    # dataset/train_sample_videos/metadata.json
+    with open(metadata_dir) as json_file:
+        videos = json.load(json_file)
+
+    # create a list to hold the data for the dataframe
+    data = []
+
+    # loop through the videos and extract the frames
+    for video_name, video_info in videos.items():
+        label = video_info['label']
+        split = video_info['split']
+        original = video_info['original']
+    #     print(video_name.rsplit('.', 1)[0])
+        for frame_name in os.listdir(dataset_dir):
+            if video_name.rsplit('.', 1)[0] in frame_name:
+                data.append({'video_name': video_name,'frame_name': frame_name, 'label': label, 'split': split})
+            
+    df = pd.DataFrame(data, columns=['video_name', 'frame_name', 'label', 'split'])
+    df.to_csv(dataframe_dir + 'df.csv')
+    # print(dataframe_dir + 'df.csv')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
