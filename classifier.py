854ab29899a30047f22cb4863a574f86e9270332
Romeo Encinares <romeoncrs@gmail.com>
2023-03-04 21:21:38 +0800

    Add train-test split to the main function

--- classifier.py
+++ classifier.py
@@ -1,6 +1,9 @@
 import argparse
 import sys
 
+import numpy as np
+from sklearn.model_selection import train_test_split
+
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--features', type=str, help='features', required=True)
@@ -15,6 +18,18 @@ def main(argv):
     features = args.features
     labels = args.labels
     output_directory = args.outputdirectory
+    
+    features = np.load(features)
+    labels = np.load(labels)
+
+    train_data, test_data, train_labels, test_labels = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+    # Print the shapes of the resulting arrays
+    print(f"Frame features in train set: {train_data.shape}")
+    print(f"Frame labels in train set: {train_labels.shape}")
+
+    print(f"Frame features in test set: {test_data.shape}")
+    print(f"Frame labels in test set: {test_labels.shape}")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
