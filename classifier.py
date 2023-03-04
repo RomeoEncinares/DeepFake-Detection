92c19bdd70c3bf00fc484b75155fb76356c06a97
Romeo Encinares <romeoncrs@gmail.com>
2023-03-04 21:22:38 +0800

    Add positional embeding layer

--- classifier.py
+++ classifier.py
@@ -2,6 +2,9 @@ import argparse
 import sys
 
 import numpy as np
+import tensorflow as tf
+from keras import layers
+from keras.layers import Input
 from sklearn.model_selection import train_test_split
 
 def parse_args(argv):
@@ -11,6 +14,26 @@ def parse_args(argv):
     parser.add_argument('--outputdirectory', type=str, help='output directory to store the models', required=True)
 
     return parser.parse_args(argv)
+
+class PositionalEmbedding(layers.Layer):
+    def __init__(self, sequence_length, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.position_embeddings = layers.Embedding(
+            input_dim=sequence_length, output_dim=output_dim
+        )
+        self.sequence_length = sequence_length
+        self.output_dim = output_dim
+
+    def call(self, inputs):
+        # The inputs are of shape: `(batch_size, frames, num_features)`
+        length = tf.shape(inputs)[1]
+        positions = tf.range(start=0, limit=length, delta=1)
+        embedded_positions = self.position_embeddings(positions)
+        return inputs + embedded_positions
+
+    def compute_mask(self, inputs, mask=None):
+        mask = tf.reduce_any(tf.cast(inputs, "bool"), axis=-1)
+        return mask
     
 def main(argv):
     args = parse_args(argv)
