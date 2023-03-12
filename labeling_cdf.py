9d892b760adb1d9830dac1a9aea0d3e0a0adf3d3
Romeo Encinares <romeoncrs@gmail.com>
2023-03-12 13:51:15 +0800

    Remove DataFrame to CSV output in favor of MySQL database integration

--- labeling_cdf.py
+++ labeling_cdf.py
@@ -6,11 +6,9 @@ import sys
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--datasetRoot', type=str, help='Dataset root directory', required=True)
-    parser.add_argument('--dataframeOutput', type=str, help='CSV dataframe output directory', required=True)
 
     return parser.parse_args(argv)
 
@@ -24,7 +22,7 @@ def sort_key(filename):
     else:
         return filename
 
-def label(frames_list, current_directory, dataframe_dir):
+def label(frames_list, current_directory):
     data = []
     for frame in frames_list:
         video_name = frame.rsplit('-', 1)[0]
@@ -34,7 +32,6 @@ def label(frames_list, current_directory, dataframe_dir):
         data.append({'video_name': video_name,'frame_name': frame_name, 'file_path': file_path,'label': label})
 
     df = pd.DataFrame(data, columns=['video_name', 'frame_name', 'file_path', 'label'])
-    df.to_csv(dataframe_dir + 'df.csv')
 
     return df
 
@@ -42,7 +39,6 @@ def main(argv):
     args = parse_args(argv)
 
     dataset_dir = args.datasetRoot
-    dataframe_dir = args.dataframeOutput
     dataset_dir_sub_root = ['Celeb-real', 'Celeb-synthesis', 'Youtube-real']
 
     le = LabelEncoder()
@@ -51,18 +47,15 @@ def main(argv):
         frames_list = os.listdir(dataset_dir + i + '/')
         if i == 'Celeb-real':
             frames_list = sorted(frames_list, key=sort_key)
-            df1 = label(frames_list, current_directory, dataframe_dir)
+            df1 = label(frames_list, current_directory)
         elif i == 'Celeb-synthesis':
             frames_list = sorted(frames_list, key=sort_key)
-            df2 = label(frames_list, current_directory, dataframe_dir)
+            df2 = label(frames_list, current_directory)
         elif i == 'Youtube-real':
             frames_list = sorted(frames_list, key=sort_key)
-            df3 = label(frames_list, current_directory, dataframe_dir)
+            df3 = label(frames_list, current_directory)
 
     df_combined = pd.concat([df1, df2, df3])
-    
-    df_combined.label = le.fit_transform(df_combined.label)
-    df_combined.to_csv(dataframe_dir + 'df.csv')
 
 if __name__ == '__main__':
     main(sys.argv[1:])
